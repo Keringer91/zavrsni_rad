28bacce9fdb43b093c9d3658605d1549163df3d4
Sabolc Keringer <keringersz@gmail.com>
2019-07-30 20:53:15 +0200

    Third commit: Database listing

---
+++ PHP/db.php
@@ -0,0 +1,13 @@
+<?php
+    try {
+        $connection = new PDO(
+            'mysql:host=127.0.0.1;dbname=blog',
+            'root',
+            ''
+        );
+
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+?>

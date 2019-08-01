fe9eede641056d18ca3803cc3ecd7713babe8417
Sabolc Keringer <keringersz@gmail.com>
2019-08-01 02:21:08 +0200

    ZADATAK 7: Latest 5 posts with links to each single post page added to the sidebar

--- aside.php
+++ aside.php
@@ -1,31 +1,36 @@
+<?php
+
+    require('db.php');
+    //SQL query to fetch the latest 5 posts (descending by date)
+    $sql = 'SELECT posts.title as title, posts.created_at as created_at, posts.body as body, posts.id as id 
+            FROM posts 
+            ORDER BY posts.created_at DESC LIMIT 5';
+
+    $stmt = $connection->prepare($sql);
+
+    $stmt->execute();
+
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    $results = $stmt->fetchAll();
+
+ 
+
+?> 
+
 <aside class="col-sm-3 ml-sm-auto blog-sidebar">
             <div class="sidebar-module sidebar-module-inset">
-                <h4>About</h4>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            </div>
-            <div class="sidebar-module">
-                <h4>Archives</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">March 2014</a></li>
-                    <li><a href="#">February 2014</a></li>
-                    <li><a href="#">January 2014</a></li>
-                    <li><a href="#">December 2013</a></li>
-                    <li><a href="#">November 2013</a></li>
-                    <li><a href="#">October 2013</a></li>
-                    <li><a href="#">September 2013</a></li>
-                    <li><a href="#">August 2013</a></li>
-                    <li><a href="#">July 2013</a></li>
-                    <li><a href="#">June 2013</a></li>
-                    <li><a href="#">May 2013</a></li>
-                    <li><a href="#">April 2013</a></li>
-                </ol>
-            </div>
-            <div class="sidebar-module">
-                <h4>Elsewhere</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">GitHub</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Facebook</a></li>
-                </ol>
+                <h4>Latest posts</h4>
+                    <div class="latest">
+                        <?php foreach($results as $r) {
+                        ?>
+                            <!--Lists all posts on the main page from the database-->
+                            <a class="blog-post-title" href="single-post.php?post_id=<?php echo $r['id']; ?>"><h5><?php echo $r['title']; ?></h5></a>
+
+                        <?php    
+                        }
+                        ?>
+                    </div>
+
             </div>
         </aside><!-- /.blog-sidebar -->
